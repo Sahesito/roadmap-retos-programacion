940e5f957581ca08bbe389cd5bc00f1d3de44dd6
PC <mar.sosaer5@gmail.com>
2025-08-01 12:40:03 -0500

    Create sahe.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/sahe.cs
@@ -0,0 +1,52 @@
+//* Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+//https://dotnet.microsoft.com/es-es/apps/aspnet/web-apps
+
+//* Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+/*
+    (//) Comentarios de una linea.
+    (/*) Comentarios de varias lineas.
+    (///) Documentación XML.
+*/
+
+//* Crea una variable (y una constante si el lenguaje lo soporta).
+string variable = "";
+const string constante = "";
+
+//* Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+int numeros = 1;
+string texto = "Hola";
+bool siNo = false;
+double decimales = 3.14;
+
+//* Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+string nombreLenguaje = "C#";
+
+Console.WriteLine($"Hola {nombreLenguaje}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
